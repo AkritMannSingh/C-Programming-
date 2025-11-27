5ca9b46befc7610e7bd9db508c44bc03dda73128
Akrit Mann Singh <akritmannsingh@gmail.com>
2025-11-27 05:56:05 +0530

    Update 01_comments.c

--- Chapter-1/01_comments.c
+++ Chapter-1/01_comments.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+int main(){
+//I write it only for kiding with understands comment 
+printf("Hello, World!");
+  // return 0 is returning 0 to OS 
+return 0;
+  /* 
+    I am Akrit Mann Singh
+    Passion: Coder
+    Language: C, Python, JavaScript, Ruby, Golang, 
+  */
+}
