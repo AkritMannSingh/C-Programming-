c716680cd6ac01c649f11afe2b02035c3b7f63a7
Akrit Mann Singh <akritmannsingh@gmail.com>
2025-11-28 06:17:38 +0530

    Create 01_datatype.c

---
+++ Chapter-1/01_datatype.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+  //int a_m_s;
+ // a_m_s = 23;
+  // Types of DataTypes - integer, float, character. 
+  int a_m_s = 23;
+
+  float b = 23.45;
+
+  char c = 'A';
+
+  printf("The value of a_m_s is %d\n", a_m_s);
+  printf("The value of b is %f\n", b 
+    );
+  printf("The value of c is %c\n", c);
+}
+
+// This code cannot run bcz it is only explantion of the datatypes.
