45d20bbc18641ce57a50d4ed6aa79dba3d55f69a
Akrit Mann Singh <akritmannsingh@gmail.com>
2025-11-29 09:40:18 +0530

    Create 01_variable.c

---
+++ Chapter-1/01_variable.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  int Akrit;
+  int Mann;
+  int Coding;
+  // int akrit mann singh; it is not allowed means -space in character not
+  // allowed in variable name.
+  int a; // variable declaration
+  a = 57317;
+  Akrit = 02;
+  Mann = 11;
+  Coding = 2011;
+  printf("The output of the program is %d", Akrit );
+  printf("%d", Mann );
+  printf( "%d", Coding);
+  return 0;
+}
