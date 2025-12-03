5f3c2e259609798963da64ad4044dc1031f36c91
Akrit Mann Singh <akritmannsingh@gmail.com>
2025-12-03 05:54:16 +0530

    Update 02_operators.c

--- Chapter-2/02_operators.c
+++ Chapter-2/02_operators.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+int main(){
+  int a = 22;
+  int b = 22*3;
+  int c = a + b;
+  printf("The value of a is %d and value of b is %d and sum is %d", a, b, c);
+  //Modulus operator is used to get the remainder from the divisio of two numbers
+  printf("The remainder when a is divided by b is %d\n", a%b);
 
+  // This does not work for exponentiation in C
+  // int d = a^b;
+  return 0;
+}
