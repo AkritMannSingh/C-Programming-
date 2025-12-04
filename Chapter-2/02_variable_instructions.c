28253b1e926302b6a9863d56fc3b00c14e4f5db9
Akrit Mann Singh <akritmannsingh@gmail.com>
2025-12-04 05:57:20 +0530

    Create 02_variable_instructions.c

---
+++ Chapter-2/02_variable_instructions.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+  int i = 10;
+  int j = i;
+  int a = 1, b = 2, c = 3, d = 5;
+  printf("The value of i is %d and value of j is %d\n", i, j);
+  printf("The value of a is %d and value of b is %d\n", a, b);
+  printf("The value of c is %d and value of d is %d", c, d);
+
+  return 0;
+}
